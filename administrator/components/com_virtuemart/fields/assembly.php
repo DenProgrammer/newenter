3fa9470aca4e6f45771a0300ccb00ba3a7b101bb
denis <fenix.programmer@gmail.com>
2015-07-22 22:19:11 +0600

    fix

--- administrator/components/com_virtuemart/fields/assembly.php
+++ administrator/components/com_virtuemart/fields/assembly.php
@@ -44,11 +44,11 @@ class JFormFieldAssembly extends JFormField {
 
         $db->setQuery('SELECT * FROM #__virtuemart_assembly');
         $assemblylist = $db->loadObjectList();
-        
-        $html         = '<select class="inputbox" multiple="multiple" name="' . $this->name . '[]" >';
+
+        $html = '<select class="inputbox" multiple="multiple" name="' . $this->name . '[]" >';
         $html .= '<option value="0">' . vmText::_('COM_VIRTUEMART_ASSEMBLY') . '</option>';
         foreach ($assemblylist as $item) {
-            $selected = in_array($item->id, $this->value) ? ' selected ' : '';
+            $selected = $this->value && in_array($item->id, $this->value) ? ' selected ' : '';
             $html .= '<option ' . $selected . ' value="' . $item->id . '">' . $item->title . '</option>';
         }
         $html .="</select>";
